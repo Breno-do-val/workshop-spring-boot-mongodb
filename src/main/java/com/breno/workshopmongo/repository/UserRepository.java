330749daf48bb42e5b16ed21f3343dab4e436b23
breno-do-val <brenorvale@gmail.com>
2020-08-04 22:48:24 -0300

    database insertion

--- src/main/java/com/breno/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/breno/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.breno.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
