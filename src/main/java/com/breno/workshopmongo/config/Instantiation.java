fb68209d8e6a20b862d5ce7ab979ce6a2ac16de9
breno-do-val <brenorvale@gmail.com>
2020-08-06 23:10:39 -0300

    Projecting author data with AuthorDTO

--- src/main/java/com/breno/workshopmongo/config/Instantiation.java
+++ src/main/java/com/breno/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.breno.workshopmongo.domain.Post;
 import com.breno.workshopmongo.domain.User;
+import com.breno.workshopmongo.dto.AuthorDTO;
 import com.breno.workshopmongo.repository.PostRepository;
 import com.breno.workshopmongo.repository.UserRepository;
 
@@ -38,8 +39,8 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.save(alex);
 		userRepository.save(bob);
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
 		postRepository.save(post1);
 		postRepository.save(post2);
